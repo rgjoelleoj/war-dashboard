afc9711effafac008c21e597e00d78f08c5bff47
rgjoelleoj <rgjoelleoj@gmail.com>
2026-03-06 18:49:47 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,22 +1,28 @@
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjNDExMmYxZC1kYWQzLTQ3OWQtYWVmZS00YWM5MWE1NjZlZWUiLCJpZCI6Mzk5MjQzLCJpYXQiOjE3NzI4MDI0MzN9.9jnH_6RJ8BWVcGMhvvbH4JKnPoRZNTEPCM9Opr77mdg"
+async function loadData(){
 
-const viewer = new Cesium.Viewer('globe')
+const res = await fetch(
+"https://YOUR-WORKER.workers.dev/api/war"
+)
 
-viewer.camera.flyTo({
-destination:Cesium.Cartesian3.fromDegrees(53,26,3000000)
-})
+const data = await res.json()
 
-async function loadStrikes(){
+document.getElementById("iran").innerText =
+data.stats.iran
 
-const res = await fetch("../data/strikes.json")
+document.getElementById("us").innerText =
+data.stats.us
 
-const strikes = await res.json()
+document.getElementById("missiles").innerText =
+data.stats.missiles
 
-strikes.forEach(s=>{
+data.strikes.forEach(s=>{
 
 viewer.entities.add({
 
-position:Cesium.Cartesian3.fromDegrees(s.lon,s.lat),
+position:Cesium.Cartesian3.fromDegrees(
+s.lon,
+s.lat
+),
 
 point:{
 pixelSize:8,
@@ -29,37 +35,6 @@ color:Cesium.Color.RED
 
 }
 
-loadStrikes()
+loadData()
 
-function launchMissile(){
-
-const start = Cesium.Cartesian3.fromDegrees(51.38,35.68)
-const end = Cesium.Cartesian3.fromDegrees(55.27,25.20)
-
-viewer.entities.add({
-
-polyline:{
-positions:[start,end],
-width:3,
-material:Cesium.Color.RED
-}
-
-})
-
-}
-
-setInterval(launchMissile,4000)
-
-async function loadStats(){
-
-const res = await fetch("../data/stats.json")
-
-const stats = await res.json()
-
-document.getElementById("iran").innerText = stats.iran
-document.getElementById("us").innerText = stats.us
-document.getElementById("missiles").innerText = stats.missiles
-
-}
-
-loadStats()
+setInterval(loadData,10000)
